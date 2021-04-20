00e7153e72a67c3cdc955b7a03413da4ad4f5277
Luke <lukebretherton@outlook.com>
2021-04-20 18:22:25 +0100

    Simple appearance, comment rendering and reply rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { List, Typography } from '@material-ui/core';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Comment from './components/Comment';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    }
+  }
 
+  componentDidMount() {
+    fetch('./data.json', {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }})
+      .then( response => response.json())
+      .then( responseJson => {
+        const comments = responseJson.data.map(item => {return item;})
+        this.setState({ data: comments });
+        console.log(this.state.data);
+      })
+  }
+
+  render() {
+    return (
+      <div className="main-view">
+        <CssBaseline />
+        <Typography variant="h3" style={{color: 'white', padding: '1%'}}>A Simple Message Board</Typography>
+        <div className="main-content-view">
+          <div className="comment-list-view">
+            <List> 
+              {this.state.data.map(commentData => {
+                return (
+                  <Comment name={commentData.user.name} comment={commentData.comment} data={commentData}/>
+                );
+              })}
+            </List>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/components/Comment.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Divider, List, ListItem, ListItemText } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import '../App.css';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        paddingTop: '1px',
+        paddingBottom: '5px',
+    },
+    listPadding: {
+        display: 'block',
+        width: '100%',
+        paddingTop: '1px',
+        paddingBottom: '1px',
+    },
+  }));
+
+
+export default function Comment(props){
+    const classes = useStyles();
+
+    // const [open, setOpen] = React.useState(true); 
+
+    // // const handleClick = () => {
+    // //     setOpen(!open);
+    // // };
+
+    // function handleReplies() {
+    //     if (props.data.replies.length > 0) {
+    //         props.data.replies.map(comment => {
+    //             console.log(comment.comment);
+    //             return (
+    //                 <ul>
+    //                     <li>{comment.user.name}</li>
+    //                     {comment.comment}
+    //                 </ul>
+    //             );
+    //         })
+    //     }
+    // }
+
+    // need to show comment & conditionally show replies
+
+    return (
+        <List dense className={classes.root}> 
+            <ListItem className={classes.listPadding}>
+                <ListItemText primary={props.name} secondary={props.comment} />
+                <Divider />
+                {props.data.replies.map(commentData => {
+                    return (
+                        <Comment name={commentData.user.name} comment={commentData.comment} data={commentData}/>
+                    );
+                })}
+            </ListItem>
+        </List>
+    )
+}
